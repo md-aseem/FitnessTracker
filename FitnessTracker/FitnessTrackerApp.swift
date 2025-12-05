1939077b6462f67a7682836da2b521c8a69747c2
mas985 <muhammad.aseem@wartsila.com>
2025-12-04 22:49:24 -0500

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Muhammad Aseem on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
