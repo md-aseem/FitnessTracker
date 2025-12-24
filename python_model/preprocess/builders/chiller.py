d85baaa2712bfcb0fdf8e6d05350c3f531172995
mas985 <muhammad.aseem@wartsila.com>
2025-12-23 23:49:35 -0500

    added specs to ChillerSpecs

--- python_model/preprocess/builders/chiller.py
+++ python_model/preprocess/builders/chiller.py
@@ -11,7 +11,6 @@ def build_chiller_specs(input_config: InputConfig, library_data) -> ChillerSpecs
     quantum = input_config.quantum
 
     is_envicool = 'envicool_55kW' in chiller_model
-
     if is_envicool and int(quantum) == 2:
         chiller_curves_dir = library_data['chillers']['envicool_q2_55kw']['chiller_curves_dir']
 
@@ -26,8 +25,20 @@ def build_chiller_specs(input_config: InputConfig, library_data) -> ChillerSpecs
     else:
         chiller_curves_df = pd.DataFrame()
 
+    chiller_data = library_data['chillers']['envicool_q2_55kw']
+
     return ChillerSpecs(
         chiller_model=chiller_model,
         chiller_noise_kit=input_config.chiller_noise_kit,
-        chiller_curves_df=chiller_curves_df
+        chiller_curves_df=chiller_curves_df,
+        battery_sensitivity= chiller_data['battery_sensitivity'],
+        inverter_sensitivity= chiller_data['inverter_sensitivity'],
+        battery_temp_target_C=chiller_data['battery_temp_target_C'],
+        inverter_temp_target_C=chiller_data['inverter_temp_target_C'],
+        electronics_aux_power=chiller_data['electronics_aux_power'],
+        pcs_pump_power_per_pcnt=chiller_data['pcs_pump_power_per_pcnt'],
+        bat_pump_power_per_prcnt=chiller_data['bat_pump_power_per_prcnt'],
+        fan_aux_power_per_pcnt=chiller_data['fan_aux_power_per_pcnt'],
+        bat_volume_flow_rate_per_prcnt=chiller_data['bat_volume_flow_rate_per_prcnt'],
+        pcs_volume_flow_rate_per_prcnt=chiller_data['pcs_volume_flow_rate_per_prcnt']
     )
--- python_model/preprocess/properties.py
+++ python_model/preprocess/properties.py
@@ -47,8 +47,26 @@ class BatterySpecs:
 class ChillerSpecs:
     chiller_model: str
     chiller_noise_kit: bool
-
     chiller_curves_df: pd.DataFrame
+
+    battery_sensitivity: float
+    inverter_sensitivity: float
+
+    # ambiguous
+    battery_temp_target_C: int
+    inverter_temp_target_C: int
+
+    electronics_aux_power: float
+
+    # these power values are multiplied by percent, from 0 to 1, to get power of each component in the code
+    pcs_pump_power_per_pcnt: float
+    bat_pump_power_per_prcnt: float
+    fan_aux_power_per_pcnt: float
+
+    # flow rate
+    bat_volume_flow_rate_per_prcnt: float
+    pcs_volume_flow_rate_per_prcnt: float
+
 
 @dataclass
 class EnvironmentSpecs:
