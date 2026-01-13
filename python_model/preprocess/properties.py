72bc81631b61ecf6e32e99ab448f25f02f0bb1d6
mas985 <muhammad.aseem@wartsila.com>
2026-01-12 21:06:09 -0500

    updated properties.py

--- python_model/preprocess/properties.py
+++ python_model/preprocess/properties.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from typing import Literal
+from pydantic import BaseModel, Field
 import numpy as np
 import pandas as pd
 from dataclasses import dataclass, field
@@ -7,7 +8,7 @@ from dataclasses import dataclass, field
 class InputConfig(BaseModel):
 
     ## high-level
-    quantum: str
+    quantum: Literal["2p0", "3p0", "2p1"]
 
     ## operational
     c_rate: float
@@ -69,28 +70,47 @@ class ChillerSpecs:
 
 @dataclass
 class WallSpecs:
-    density: float
+    rho: float
     area: float
     mass: float
     cp: float
-    conductivity: float
+    k: float
     thickness: float
-    # R: field(default_factory=lambda: np.ones(3)) # 3 nodes # better to define it in the simulation?
     innerUA: float = 8.0
     outerUA: float = 8.0
+
+    def __post_init__(self):
+        self.dx = self.thickness / 7 # 7 nodes hardcoded into the code for now
+        self.R =  np.array([self.area * (1 / ((1 / self.outerUA) + ((self.dx / 2) / self.k))),  # wall-to-ambient thermal conductance
+                            self.area * self.k / self.dx,                                       # node-to-node thermal conductance
+                            self.area * (1 / ((1 / self.innerUA) + ((self.dx / 2) / self.k)))]) # # inner-to-wall thermal conductance
 
 @dataclass
 class EnvironmentSpecs:
     ambient_temperature: float
     sunrise_time: int
     sunset_time: int
+    is_radiation: bool = True
+    radiation_intensity: float = 350 # W/m2  radiation_intensity(1000) * absorptivity(0.35)
+    percent_surface_in_radiation: float = 1
 
+@dataclass
+class ContainerSpecs:
+    container_wall_area: float
+    radiation_surface_prcnt: float
+    container_wall_UA: float
+    container_wall_prcnt_steel: float
 
 @dataclass
 class OperationalSpecs:
     time_s: np.ndarray
     current_profile: np.ndarray
     ambient_profile: np.ndarray
+    radiation_profile: np.ndarray
+
+    def __post_init__(self):
+        self.n = self.current_profile.shape[0]
+        self.dt = np.diff(self.time_s)[0]
 
 
 ### combined system specs
@@ -99,6 +119,7 @@ class SystemSpecs:
 
     battery_specs: BatterySpecs
     chiller_specs: ChillerSpecs
+    container_specs: ContainerSpecs
     steel_wall_specs: WallSpecs # wall 1
     insulation_wall_specs: WallSpecs # wall 2
 
