5fadcea6d6f77a04eab0b5003301e10d38b4a05f
mas985 <muhammad.aseem@wartsila.com>
2026-02-04 23:38:17 -0500

    fixed some timestepping issues and capping issues

--- python_model/simulation.py
+++ python_model/simulation.py
@@ -308,7 +308,7 @@ class Simulation:
         # Connected to Chiller/Coolant state
         # Using values from control logic
         # 480 LPM max * pump percent
-        volume_flow_rate_lpm = 480.0 * self.battery_pump_pcnt[i]
+        volume_flow_rate_lpm = self.BAT_VOLUME_FLOW_RATE_LPM * self.battery_pump_pcnt[i]
         
         if volume_flow_rate_lpm < 0.1:
             coolant_mass_flow = 0.001
@@ -318,7 +318,7 @@ class Simulation:
         coolant_cp = 3400.0 # J/kgK
         
         # Temp leaving chiller (entering cold plate)
-        tlc_last = self.battery_stream_temp_leaving_chiller[i] # This comes from update_chiller_condition_and_cool
+        tlc_last = self.battery_stream_temp_leaving_chiller[i-1]
         
         # Max Enthalpy Delta
         # maxEnthalpyDelta = c->batteryStream->massFlowRate*c->batteryStream->coolantCp*(b->tempLast[0] - c->batteryStream->tlcLast);
@@ -329,7 +329,7 @@ class Simulation:
         bottom_q = -0.4 * max_possible_heat_transfer + (self.battery_temp[i-1, 1] - self.battery_temp[i-1, 0]) * b_specs.R[1]
         
         # topQ    = (simmain->internalAirTemp - b->tempLast[6])*R[2] + (b->tempLast[5] - b->tempLast[6])*R[1];
-        top_q = (self.internal_air_temp[i-1] - self.battery_temp[i-1, 6]) * b_specs.R[2] + \
+        top_q = (self.internal_air_temp[i] - self.battery_temp[i-1, 6]) * b_specs.R[2] + \
                 (self.battery_temp[i-1, 5] - self.battery_temp[i-1, 6]) * b_specs.R[1]
                 
         # Heat into Chiller (for next step calculation of chiller return temp)
@@ -342,7 +342,7 @@ class Simulation:
         if coolant_mass_flow > 0.001:
             self.battery_stream_temp_entering_chiller[i] = tlc_last - heat_into_cold_plate / (coolant_mass_flow * coolant_cp)
         else:
-            self.battery_stream_temp_entering_chiller[i] = tlc_last # No flow, no change? Or stays at last temp?
+            self.battery_stream_temp_entering_chiller[i] = tlc_last
 
         # Node 0 (Bottom)
         # b->temperature[0] += (bottomQ + (ONE/7.0)*batteryTotalHeatGeneration(simmain))*transientDt/(b->mass * b->cp / 7.0);
@@ -386,7 +386,7 @@ class Simulation:
         
         current_amps = self.current_profile[i]
         c_rate = current_amps / self.system_specs.battery_specs.cell_capacity_ah
-        
+
         soc = self.soc[i]
         cell_heat = self.heat_gen_interpolator((soc, c_rate))
         
@@ -609,15 +609,11 @@ class Simulation:
 
 
     def calculate_battery_stream_mass_flow_rate(self, i):
+        # Clip pump percentage at 0.01 to match C model's floor speed
+        pump_pcnt = np.clip(self.battery_pump_pcnt[i], 0.01, 1.0)
         
-        # volume_flow_rate_lpm = 400.0 * self.battery_pump_pcnt[i] # Assuming 400 LPM max
-
-        if self.battery_pump_pcnt[i] < 0.1:
-            battery_stream_mass_flow = 0.001
-            volume_flow_rate_lpm = 0.1
-        else:
-            volume_flow_rate_lpm = self.BAT_VOLUME_FLOW_RATE_LPM * self.battery_pump_pcnt[i]
-            battery_stream_mass_flow = (volume_flow_rate_lpm / 60000.0) * 1050.0 # kg/s (~8.4 kg/s max?)
+        volume_flow_rate_lpm = self.BAT_VOLUME_FLOW_RATE_LPM * pump_pcnt
+        battery_stream_mass_flow = (volume_flow_rate_lpm / 60000.0) * 1050.0 # kg/s (~8.4 kg/s max)
 
         return battery_stream_mass_flow
 
