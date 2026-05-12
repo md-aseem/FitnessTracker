858f78629dfcfb4f80b5df89df0b5a90975a2ac2
mas985 <muhammad.aseem@wartsila.com>
2026-05-12 17:53:49 -0400

    decluttering

--- python_model/preprocess/builders/container.py
+++ python_model/preprocess/builders/container.py
@@ -9,5 +9,4 @@ def build_container_specs(input_config: InputConfig, library_data) -> ContainerS
 
     return ContainerSpecs(container_wall_area = library_data['containers'][quantum]['container_wall_area'],
                           radiation_surface_prcnt= library_data['containers'][quantum]['radiation_surface_prcnt'],
-                          container_wall_UA = library_data['containers'][quantum]['container_wall_UA'],
                           container_wall_prcnt_steel = library_data['containers'][quantum]['container_wall_prcnt_steel'],)
