9abbf31e84d96a69316c41b5cc12e4c204249471
mas985 <muhammad.aseem@wartsila.com>
2026-05-14 10:06:44 -0400

    make it a package

--- scratch/test_multi_year_6mo.py
+++
@@ -1,7 +0,0 @@
-from quantum_thermal_model.simulation_multi_year import SimulationMultiYear
-import os
-
-if __name__ == "__main__":
-    # Override n_years for testing
-    sim = SimulationMultiYear(n_years=2)
-    sim.run()
