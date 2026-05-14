9992b42a9a894f0b0f2c0aa8e793fe36f83b4b2c
mas985 <muhammad.aseem@wartsila.com>
2026-05-14 10:44:45 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setuptools.setup(
     name="quantum_thermal_model",
-    version="0.1.0",
+    version="0.1.1",
     author="Wärtsilä North America, Inc.",
     description="Quantum Thermal Model for battery systems",
     long_description_content_type="text/markdown",
